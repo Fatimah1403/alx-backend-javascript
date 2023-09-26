75d1b975072272633bd612891e4c9d7c2087165f
Fatimah1403 <fattyhassan14@gmail.com>
2023-09-26 02:38:41 +0000

    Task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   const myarray = [];
-  for (const item of object.values(report.allEmployees)) {
+  for (const item of Object.values(report.allEmployees)) {
     myarray.push(...item);
   }
 
