0e7659a1cbec0249f884dc96d13d1c6bc18457ce
Fatimah1403 <fattyhassan14@gmail.com>
2023-09-28 12:57:39 +0000

    Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,10 @@
-export default function handleResponseFromAPI(promise){
-    const body =  { status: 200, body: 'Success' };
-    
-    return promise
-      .then(() => body)
-      .catch(() => new Error())
-      .finally(() => { console.log('Got a response from the API'); });               
+export default function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
