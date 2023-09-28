7c23cbedf9f819080cda45206672fa2262d3b1be
Fatimah1403 <fattyhassan14@gmail.com>
2023-09-28 10:58:26 +0000

    Task 0 update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {}); 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("API response data is true");
+        }, 1000);
+    }); 
 }
