2bf0712f1ea97174e27845013eceae57ec8e91b2
Fatimah1403 <fattyhassan14@gmail.com>
2023-09-29 02:24:01 +0000

    Task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,30 +1,30 @@
 export default class Pricing {
-    constructor(amount, currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+   // getter for amouunt;
+  get amount() {
+    return this._amount;
+  }
+   // setter for amount
+  set amount(value) {
+    this._amount = value;
+  }
+   // getter for currency
+  get currency() {
+    return this._currency;
+  }
 
-    // getter for amouunt;
-    get amount() {
-        return this._amount;
-    }
-    // setter for amount
-    set amount(value) {
-        this._amount = value;
-    }
+  set currency(values) {
+    this._currency = values;
+  }
 
-    // getter for currency
-    get currency() {
-        return this._currency;
-    }
-    // setter for currency
-    set currency(value) {
-        this._currency = value;
-    }
-    displayFullPrice() {
-        return `${this.amount} ${this.currency._name} (${this.currency._code})`;
-    }
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
 }
