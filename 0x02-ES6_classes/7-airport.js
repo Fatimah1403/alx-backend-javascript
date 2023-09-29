34e5e5b1b7ebbda9d698e6d7f27b06827e07a7db
Fatimah1403 <fattyhassan14@gmail.com>
2023-09-29 16:44:27 +0000

    Task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+extend default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  } 
+  
+  get code() {
+  return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+ 
+  toString() {
+    return ['object ${this.code}'];
+  }
+}
+
