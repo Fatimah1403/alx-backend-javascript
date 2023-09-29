ee40674a135c3f69d7bed2f576d99c02b518dded
Fatimah1403 <fattyhassan14@gmail.com>
2023-09-29 02:10:33 +0000

    Task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,40 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string' && name !== 'string') {
-      throw new TypeError('Invalid input type')
-    }
     this._code = code;
     this._name = name;
   }
-  
-  // Getter for code
-  get code() {
-    return this._code;
-  }
 
-  // Setter for code
-  set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = value;
-  }
-
-   // Getter for name
   get name() {
     return this._name;
   }
 
-   // Setter for name
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = value;
+  set name(n) {
+    this._name = n;
   }
 
-  // method to display currency.
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    this._name = c;
+  }
+
   displayFullCurrency() {
-  return `${this.name} (${this.code})`;
+    return `${this.name} (${this.code})`;
   }
 }
