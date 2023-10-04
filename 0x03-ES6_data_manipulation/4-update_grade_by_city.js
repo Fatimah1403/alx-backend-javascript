cda270e88ec639b62e3703e7792823b4128ae97c
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-04 02:05:20 +0000

    Task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  const studentCity = list.filter((student) => student.location === city);
+  const updateStuGrades = studentCity.map((student) => {
+    const gradeObj = newGrades.find(grades => grade.studentId === student.id);
+    return {
+      ...student,
+      grade: gradeObj ? gradeObj.grade : 'N/A',
+    };
+  });
+  return updateStuGrades;
+}
