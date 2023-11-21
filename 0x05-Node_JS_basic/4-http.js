086b4861df12b0876a8a0b726e33a8fc703cabc6
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-21 18:36:54 -0500

    Server created

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,17 @@
-const http = require("http");
-// const hostname = "127.0.0.1";
+const http = require('http');
+const hostname = "127.0.0.1";
 const port = 1245;
 
 const app = http.createServer((req, res) => {
     const header = {
-        "Content-Type": "text/plain"
+        'Content-Type': 'text/plain'
     };
     res.writeHead(200, header);
-    res.end("Hello Holberton School!\n");
+    res.end('Hello Holberton School!\n');
 });
 
-app.listen(port )
- 
+app.listen(port, hostname, () => {
+    console.log(`Server is listening at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
