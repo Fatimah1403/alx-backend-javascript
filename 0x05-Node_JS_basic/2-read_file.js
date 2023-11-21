a523756344b7b166418e3941d6cc21f7ebc0f762
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-21 16:53:19 -0500

    new changes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,27 +1,29 @@
-const fs = require('fs');
+const fs = require("fs");
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
-  try {
-    const fileContents = fs.readFileSync(fileName, 'utf-8');
-    const lines = fileContents.toString().split('\n');
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i]) {
-        length += 1;
-        const field = lines[i].toString().split(',');
-        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students[field[3]].push(field[0]);
-        } else {
-          students[field[3]] = [field[0]];
+function countStudents(path) {
+   try {
+    const students = {};
+    const fields = {};
+    let length = 0;
+
+    const fileContents = fs.readFileSync(path, "utf-8");
+    const dataContent = fileContents.toString().split("\n");
+
+    for (let i = 0; i < dataContent.length; i += 1) {
+        if (dataContent[i]) {
+            length += 1;
+            const field = dataContent[i].toString().split(",");
+            if (field[3] in students) {
+                students[field[3]].push(field[0]);
+            } else {
+                students[field[3]] = [field[0]];
+            }
+            if (field[3] in fields) {
+                fields[field[3]] += 1;
+            } else {
+                fields[field[3]] = 1;
+            }
         }
-        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-          fields[field[3]] += 1;
-        } else {
-          fields[field[3]] = 1;
-        }
-      }
     }
     const l = length - 1;
     console.log(`Number of students: ${l}`);
@@ -33,6 +35,7 @@ function countStudents(fileName) {
   } catch (error) {
     throw Error('Cannot load the database');
   }
+
 }
 
 module.exports = countStudents;
