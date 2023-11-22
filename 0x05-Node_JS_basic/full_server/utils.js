a465c331c03cd7bd89989e5934d703ea14c89db2
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-22 13:10:46 -0500

    utils file updated

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,26 +1,40 @@
-const { readFile } = require('fs');
+const fs = require('fs').promises;
 
-module.exports = function readDatabase(filePath) {
-  const students = {};
-  return new Promise((resolve, reject) => {
-    readFile(filePath, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const lines = data.toString().split('\n');
-        const noHeader = lines.slice(1);
-        for (let i = 0; i < noHeader.length; i += 1) {
-          if (noHeader[i]) {
-            const field = noHeader[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
-            } else {
-              students[field[3]] = [field[0]];
-            }
-          }
+async function readDatabase(fileName) {
+  try {
+    const students = {};
+    const fields = {};
+
+    const fileContents = await fs.readFile(fileName, 'utf-8');
+    const lines = fileContents.trim().split('\n');
+
+    for (let i = 0; i < lines.length; i += 1) {
+      const line = lines[i].trim();
+      if (line) {
+        const field = line.split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
         }
-        resolve(students);
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
       }
-    });
-  });
-};
+    }
+
+    let result = (`Number of students: ${lines.length - 1}`);
+
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        result += (`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
+    }
+    return result.trim();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = readDatabase;
