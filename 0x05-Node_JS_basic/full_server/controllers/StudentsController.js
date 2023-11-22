c4d99afaed846f06611b5c49aef0b71d13e55621
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-22 14:18:45 -0500

    New changes

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,7 +1,7 @@
 const readDatabase = require('../utils');
 
 class StudentsController {
-  static getAllStudents(request, response) {
+  static getAllStudents(req, res) {
     readDatabase(process.argv[2].toString()).then((students) => {
       const output = [];
       output.push('This is the list of our students');
@@ -10,22 +10,22 @@ class StudentsController {
       for (let i = 0; i < keys.length; i += 1) {
         output.push(`Number of students in ${keys[i]}: ${students[keys[i]].length}. List: ${students[keys[i]].join(', ')}`);
       }
-      response.status(200).send(output.join('\n'));
+      res.status(200).send(output.join('\n'));
     }).catch(() => {
-      response.status(500).send('Cannot load the database');
+      res.status(500).send('Cannot load the database');
     });
   }
 
-  static getAllStudentsByMajor(request, response) {
-    const field = request.params.major;
+  static getAllStudentsByMajor(req, res) {
+    const field = req.params.major;
     readDatabase(process.argv[2].toString()).then((students) => {
       if (!(field in students)) {
-        response.status(500).send('Major parameter must be CS or SWE');
+        res.status(500).send('Major parameter must be CS or SWE');
       } else {
-        response.status(200).send(`List: ${students[field].join(', ')}`);
+        res.status(200).send(`List: ${students[field].join(', ')}`);
       }
     }).catch(() => {
-      response.status(500).send('Cannot load the database');
+      res.status(500).send('Cannot load the database');
     });
   }
 }
