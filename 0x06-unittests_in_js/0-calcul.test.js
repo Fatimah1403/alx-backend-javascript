8e3b105f86d5c309fbe7c89534275ed349b2fce2
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-23 18:21:29 -0500

    new changes

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,12 +8,20 @@ describe('calculateNumber()', () => {
         assert.strictEqual(output, 8);
     });
     it(message, () => {
-        const output = calculateNumber(20, 10);
-        assert.strictEqual(output, 30);
+        const output = calculateNumber(20, -10);
+        assert.strictEqual(output, 10);
     });
     it(message, () => {
-        const output = calculateNumber(-1.5, -2.5);
-        assert.strictEqual(output, -3);
+        const output = calculateNumber(-50, 10);
+        assert.strictEqual(output, -40);
+    });
+    it(message, () => {
+        const output = calculateNumber(-1, -3);
+        assert.strictEqual(output, -4);
+    });
+    it(message, () => {
+        const output = calculateNumber(0, 0);
+        assert.strictEqual(output, 0);
     });
     const msg = 'should handle large numbers and return the sum of rounded numbers';
     it(msg, () => {
