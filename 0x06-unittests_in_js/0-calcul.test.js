828f0d65b76d39420586c2ed46fbbdaf800c9cc6
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-23 17:56:58 -0500

    Rounded sum added

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber()', function() {
+    const message = 'should return the rounded sum of a and b';
+    it(message, () => {
+        const output = calculateNumber(2.5, 4.5);
+        assert.strictEqual(output, 8);
+    });
+    it(message, () => {
+        const output = calculateNumber(20, 10);
+        assert.strictEqual(output, 30);
+    });
+    const msg = 'should handle large numbers and return the sum of rounded numbers';
+    it(msg, () => {
+        const output = calculateNumber(123456789.789, 987654321.321);
+        assert.strictEqual(output, 1111111111);
+    });
+});
